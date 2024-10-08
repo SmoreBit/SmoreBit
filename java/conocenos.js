c2b7c87c11c6bbc61805701af490a399edf8b19e
GUILLERMO ESPIRITU MARTINEZ <espiritu.martinez.guillermo@gmail.com>
2024-10-08 13:20:07 -0600

    Commitcito, página mejorada

---
+++ java/conocenos.js
@@ -0,0 +1,21 @@
+function toggleInfo(element) {
+    const info = element.querySelector('.team-info');
+    info.classList.toggle('expanded');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetUrl = this.href;
+
+            document.body.classList.add('fade-out');
+
+            setTimeout(() => {
+                window.location.href = targetUrl;
+            }, 500); // Tiempo de la animación en milisegundos
+        });
+    });
+});
---
+++ java/index.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('visible');
+    } else {
+        navbar.classList.remove('visible');
+    }
+};
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetUrl = this.href;
+
+            document.body.classList.add('fade-out');
+
+            setTimeout(() => {
+                window.location.href = targetUrl;
+            }, 500); // Tiempo de la animación en milisegundos
+        });
+    });
+});
