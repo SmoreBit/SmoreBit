84adab478b82bffda7e3ff320b243339b0b8483d
SmoreBit <smorebit@gmail.com>
2024-11-20 00:35:54 -0600

    Delete Bluefin directory

--- Bluefin/vista_usuario/cerrarsesion.js
+++
@@ -1,5 +0,0 @@
-        function cerrarSesion() {
-            if (confirm("¿Estás seguro de que quieres cerrar la sesión?")) {
-                window.location.href = "seguridad/cerrarSesion2.jsp";
-            }
-        }
